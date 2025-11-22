03945207d0bbf803325b1c0ec78321f93ca38003
[dyad] <signups.mathx@gmail.com>
2025-11-23 10:45:30 +1100

    [dyad] Adding background animations to the landing page - wrote 1 file(s)

--- App.tsx
+++ App.tsx
@@ -19,12 +19,27 @@ const AppContent: React.FC = () => {
         );
     }
 
+    const isLandingPage = !session && !showLogin;
+
     return (
         <>
             {/* Shared Background Elements */}
             <div className="fixed inset-0 grid-bg z-0"></div>
             <div className="fixed top-0 left-1/2 -translate-x-1/2 -translate-y-1/2 glow-bg"></div>
             <div className="fixed bottom-0 right-0 translate-x-1/3 translate-y-1/3 glow-bg bg-purple-900/20"></div>
+
+            {/* Landing Page Specific Animations */}
+            {isLandingPage && (
+                <div className="absolute inset-0 z-0 pointer-events-none overflow-hidden">
+                    <div className="relative w-full h-full">
+                        <div className="absolute w-[400px] h-[400px] bg-blue-900/10 blur-[80px] rounded-full top-1/4 left-1/4 animate-float-1"></div>
+                        <div className="absolute w-[300px] h-[300px] bg-indigo-900/10 blur-[70px] rounded-full bottom-1/3 right-1/4 animate-float-2"></div>
+                        <div className="absolute w-[350px] h-[350px] bg-cyan-900/10 blur-[90px] rounded-full top-1/3 right-1/3 animate-float-3"></div>
+                        <div className="absolute w-[250px] h-[250px] bg-purple-900/10 blur-[60px] rounded-full bottom-1/4 left-1/3 animate-float-4"></div>
+                        <div className="absolute w-[300px] h-[300px] bg-slate-800/20 blur-[80px] rounded-full top-20 right-20 animate-float-5"></div>
+                    </div>
+                </div>
+            )}
 
             {session ? (
                 <ChatView />
